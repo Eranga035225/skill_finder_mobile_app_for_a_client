139bcd74f64d3fb1526764579b01234b91c7231e
Eranga <erangakavishanka@icloud.com>
2025-11-14 15:29:40 +0530

    setting up the home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,15 +11,36 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body:
       Column(
         children: [
           Image(image: AssetImage('assets/images/homescreen.png', ),fit:BoxFit.fitWidth,
           width:double.infinity ),
-          Text('Find and Hire local service Providers', style: TextStyle(
-            color: Colors.black
-          ),),
-          Text('Connect with skilled professionals for all your home service needs', style:TextStyle(color:Colors.black))
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal:0, vertical:8),
+            child: Column(
+              children: [
+                Text('Find and hire local service providers', 
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 28,
+                  fontWeight: FontWeight.w500,
+                  
+                ),),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Text('Connect with skilled professionals for all your home service needs', 
+            style:TextStyle(color:Colors.grey.shade800,
+            fontSize: 17
+            ),
+            textAlign: TextAlign.center,),
+          )
+
           
           
           
