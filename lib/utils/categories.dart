66e96ab4aad4ce0e5cba81267708bd54937d6c6f
Eranga <erangakavishanka@icloud.com>
2025-11-17 22:48:52 +0530

    created demo data categories file

--- lib/utils/categories.dart
+++ lib/utils/categories.dart
@@ -1,5 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import '../models/category_model.dart';
+
 class Categories{
-  Static categorie
+  static List<Category> categories = [
+    Category(type: 'Carpenters', icon: Icon(Icons.handyman)),
+    Category(type:'Electricians', icon: Icon(Icons.electrical_services_outlined)),
+    Category(type:'Carpenters',  icon: Icon(Icons.handyman)),
+    Category(type: 'Plumbers' , icon: Icon(Icons.handyman)),
+    Category(type:'Masons', icon: Icon(Icons.electrical_services_outlined)),
+    Category(type:'Gardenrs',  icon: Icon(Icons.handyman))
+
+
+
+
+
+
+
+
+
+  ] ;
 
 
 
